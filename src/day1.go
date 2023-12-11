5f627e6c2bdcf6be1816540976aceafcf41ba8c2
Wellington Junior <wdiasjunior@gmail.com>
2023-12-10 22:16:11 -0300

    day 1

--- src/day1.go
+++ src/day1.go
@@ -2,8 +2,42 @@ package src
 
 import (
   "fmt"
+  "os"
+  "bufio"
+  "unicode"
+  "strconv"
 )
 
 func Day1() {
-  fmt.Println("advent of code 2023")
+  data, _ := os.Open("src/day1_input.txt")
+
+  defer data.Close()
+
+  scanner := bufio.NewScanner(data)
+
+  var result int
+
+  for scanner.Scan() {
+    // fmt.Println(scanner.Text())
+
+    acc := ""
+    for _, char := range scanner.Text() {
+      if unicode.IsDigit(char) {
+        acc = acc + string(char)
+      }
+    }
+
+    if len(acc) == 1 {
+      acc = acc + acc
+    } else if len(acc) > 2 {
+      lastValue := acc[len(acc)-1:]
+      acc = acc[0:1] + lastValue
+    }
+
+    // fmt.Println(acc)
+    accInt, _ := strconv.Atoi(acc)
+    result = result + accInt
+  }
+
+  fmt.Println(result)
 }
