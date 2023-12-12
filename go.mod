b06488782d5b9b086f14af6aabfe069420caf4c4
Wellington Junior <wdiasjunior@gmail.com>
2023-12-11 21:57:48 -0300

    check the branches

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/wdiasjunior/advent-of-code
-
-go 1.21.4
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-  src "github.com/wdiasjunior/advent-of-code/src"
-)
-
-func main() {
-  src.Day1Part2();
-}
